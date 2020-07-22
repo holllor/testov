3a0d5f1c78beb3e9023594309b4930a0c4e78d7d
oleg <oleg@olegKubuntu>
2020-07-22 21:51:08 +0300

    first

---
+++ src/test/java/ru/home/test/testov/TestovApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.home.test.testov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
